9c7200d5cdd9e70da7ef29c893538bdb3f82b5e0
Robbie <robhanson27@gmail.com>
2025-02-07 14:35:10 +0000

    Update __init__.py

--- CompGIT/__init__.py
+++ CompGIT/__init__.py
@@ -1,3 +1,26 @@
-from sage.all_cmdline import *
+from sage.all import *
+from sage.all import WeylGroup #experimental purposes only
 
-# rest coming soon
+from CompGIT import SimpleGroup
+from CompGIT import Git
+
+from SimpleGroup import (OneParamSubgroup,
+                         upper_triangular_entries,
+                         lower_triangular_entries,
+                         one_param_subgroup,
+                         A_coord_change_from_T_to_H,
+                         inverse_of_upper_triangular,
+                         A_cone_basis_constructor,
+                         A_cone_basis_constructor_from_T,
+                         A_T_basis_constructor_from_gamma,
+                         D_cone_basis_constructor,
+                         D_T_basis_constructor_from_gamma,
+                         B_fundamental_weight_constructor,
+                         D_fundamental_weight_constructor,
+                         SimpleGroup)
+
+from Git import (proportional,
+                 weights_matrix,
+                 averageWeight,
+                 timedRunProblem,
+                 GITProblem)
