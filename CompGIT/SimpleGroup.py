a99f795d3deda21ed7b63041431e5644f5618568
Robbie <robhanson27@gmail.com>
2025-03-03 16:06:26 +0000

    Update SimpleGroup.py
    Nicer white spacing for Dynkin type E, F, G matrices

--- CompGIT/SimpleGroup.py
+++ CompGIT/SimpleGroup.py
@@ -412,28 +412,51 @@ class SimpleGroup(object):
             self.T_to_H_change = matrix.identity(QQ, rnk) #Change of coordinate matrix from T-coordinates to H-coordinates. Returns T-vectors in H-coordinates.
  
          elif Dynkin_type == 'E' and rnk == 6:
-            self.cone_basis = matrix.transpose(QQ[sqrt(3)], [[sqrt(3), 0, 0, 0, 0, 1], [1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1], [1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, -1/3*sqrt3, 1], [1/2*sqrt3, 1/2*sqrt3, 0, 0, 0, 1], [1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, 0, 0, 1], [0, 0, 0, 0, 0, 1]] )
+            self.cone_basis = matrix.transpose(QQ[sqrt(3)], [[sqrt(3), 0, 0, 0, 0, 1], 
+                                                             [1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1/5*sqrt3, 1], 
+                                                             [1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, -1/3*sqrt3, 1], 
+                                                             [1/2*sqrt3, 1/2*sqrt3, 0, 0, 0, 1], 
+                                                             [1/3*sqrt3, 1/3*sqrt3, 1/3*sqrt3, 0, 0, 1], 
+                                                             [0, 0, 0, 0, 0, 1]] )
             self.T_to_gamma_change = self.cone_basis.inverse()
             self.T_to_H_change = matrix.identity(QQ[sqrt(3)], 6)
             
          elif Dynkin_type == 'E' and rnk == 7:
-            self.cone_basis = matrix.transpose(QQ[sqrt(2)], [[1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), -1/4*sqrt(2), 1], [1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1], [0, 0, 0, 0, 0, 0, 1], [sqrt(2), 0, 0, 0, 0, 0, 1], [1/2*sqrt(2), 1/2*sqrt(2), 0, 0, 0, 0, 1], [1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 0, 0, 1], [1/3*sqrt(2), 1/3*sqrt(2), 1/3*sqrt(2), 0, 0, 0, 1]]  )
+            self.cone_basis = matrix.transpose(QQ[sqrt(2)], [[1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), -1/4*sqrt(2), 1], 
+                                                             [1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1/6*sqrt(2), 1], 
+                                                             [0, 0, 0, 0, 0, 0, 1], 
+                                                             [sqrt(2), 0, 0, 0, 0, 0, 1], 
+                                                             [1/2*sqrt(2), 1/2*sqrt(2), 0, 0, 0, 0, 1], 
+                                                             [1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 1/4*sqrt(2), 0, 0, 1],
+                                                             [1/3*sqrt(2), 1/3*sqrt(2), 1/3*sqrt(2), 0, 0, 0, 1]] )
             self.T_to_gamma_change = self.cone_basis.inverse()
             self.T_to_H_change = matrix.identity(QQ[sqrt(2)], 7)
             
          elif Dynkin_type == 'E' and rnk == 8:
-            self.cone_basis = matrix.transpose(QQ, [[-1, 0, 0, 0, 0, 0, 0, 1], [-1/3, -1/3, -1/3, 0, 0, 0, 0, 1], [-1/7, -1/7, -1/7, -1/7, -1/7, -1/7, -1/7, 1], [0, 0, 0, 0, 0, 0, 0, 1], [-1/5, -1/5, -1/5, -1/5, -1/5, -1/5, 1/5, 1], [-1/2, -1/2, 0, 0, 0, 0, 0, 1], [-1/5, -1/5, -1/5, -1/5, -1/5, 0, 0, 1], [-1/4, -1/4, -1/4, -1/4, 0, 0, 0, 1]])
+            self.cone_basis = matrix.transpose(QQ, [[-1, 0, 0, 0, 0, 0, 0, 1], 
+                                                    [-1/3, -1/3, -1/3, 0, 0, 0, 0, 1], 
+                                                    [-1/7, -1/7, -1/7, -1/7, -1/7, -1/7, -1/7, 1], 
+                                                    [0, 0, 0, 0, 0, 0, 0, 1], 
+                                                    [-1/5, -1/5, -1/5, -1/5, -1/5, -1/5, 1/5, 1], 
+                                                    [-1/2, -1/2, 0, 0, 0, 0, 0, 1], 
+                                                    [-1/5, -1/5, -1/5, -1/5, -1/5, 0, 0, 1], 
+                                                    [-1/4, -1/4, -1/4, -1/4, 0, 0, 0, 1]])
             self.T_to_gamma_change = self.cone_basis.inverse()
             self.T_to_H_change = matrix.identity(QQ, 8)
             
           elif Dynkin_type == 'F':
-            self.cone_basis = matrix.transpose(QQ, [[1, 1, 0, 0], [1, 0, 0, 0], [3, 1, 1, 1], [1, 1/2, 1/2, 0]])
+            self.cone_basis = matrix.transpose(QQ, [[1, 1, 0, 0], 
+                                                    [1, 0, 0, 0], 
+                                                    [3, 1, 1, 1], 
+                                                    [1, 1/2, 1/2, 0]])
             self.T_to_gamma_change = self.cone_basis.inverse()
             self.T_to_H_change = matrix.identity(QQ, 4)
  
         elif Dynkin_type == 'G':
-            self.cone_basis = matrix(QQ[sqrt(3)], [[1, sqrt(3)], [0, 1]]) #stores rays of the fundamental chamber in H-coordinates. Change of coordinate matrix from gamma-coordinates to H-coordinates.
-            self.T_to_gamma_change = matrix(QQ[sqrt(3)], [[1, -sqrt(3)], [0, 1]]) # inverse matrix to self.cone_basis. Change of coordinate matrix from T-coordinates to gamma-coordinates. Returns T-vectors in gamma-coordinates.
+            self.cone_basis = matrix(QQ[sqrt(3)], [[1, sqrt(3)], 
+                                                   [0, 1]]) #stores rays of the fundamental chamber in H-coordinates. Change of coordinate matrix from gamma-coordinates to H-coordinates.
+            self.T_to_gamma_change = matrix(QQ[sqrt(3)], [[1, -sqrt(3)], 
+                                                          [0, 1]]) # inverse matrix to self.cone_basis. Change of coordinate matrix from T-coordinates to gamma-coordinates. Returns T-vectors in gamma-coordinates.
             self.T_to_H_change = matrix.identity(QQ[sqrt(3)], 2)
         
         else:
