72c27accb1fd8ccef951988e80c1c29043a4e46c
Robbie <robhanson27@gmail.com>
2025-03-18 11:24:49 +0000

    Update projections.py

--- projections.py
+++ projections.py
@@ -4,25 +4,24 @@ from sage.rings.rational_field import QQ
 from sage.modules.free_module_element import vector
 from sage.all import *
 
-# Input simple roots sets for G2, F4, E6, E7, E8
+# Choices of simple roots sets for G2, F4, E6, E7, E8 are stored as matrices in which:
+# - columns are choices of simple roots in a specified dimension
+# - in higher dimensional cases, an orthogonal vector is appended in the last column
+# - in lower dimensional cases, a zero vector is appended in the last column
 
 simplerootsG2dim3 = matrix(QQ[sqrt(3)], 
-                           [[1,  -1], 
-                            [-1,  2], 
-                            [0 , -1]])
+                           [[1,  -1, 1],
+                            [-1,  2, 1],
+                            [0 , -1, 1]])
 
 simplerootsG2dim2 = matrix(QQ[sqrt(3)], 
-                           [[1, 0], 
-                            [-3/2, sqrt(3)/2]])
+                           [[1, 0, 0],
+                            [-3/2, sqrt(3)/2, 0]])
 
-simplerootsF4dim4 = matrix(QQ, 
-                           [[0,1,-1,0], 
-                            [0,0,1,-1], 
-                            [0,0,0,1], 
-                            [1/2, -1/2, -1/2, -1/2]])
-print(simplerootsF4, '\n')
 
-simplerootsE6dim6 = matrix(QQ[sqrt(3)], 
+# To-do: input simplerootsE6dim8
+
+simplerootsE6dim6 = matrix(QQ[sqrt(3)],
                                    [[1,-1,0,0,0,0], 
                                    [0,1,-1,0,0,0], 
                                    [0,0,1,-1,0,0], 
@@ -30,7 +29,9 @@ simplerootsE6dim6 = matrix(QQ[sqrt(3)],
                                    [0,0,0,1,1,0], 
                                    [-1/2, -1/2, -1/2, -1/2, -1/2, sqrt(3)/2]])
 
-simplerootsE7dim7 = matrix(QQ[sqrt(2)], 
+# To-do: input simplerootsE7dim8
+
+simplerootsE7dim7 = matrix(QQ[sqrt(2)],
                                    [[1,-1,0,0,0,0,0], 
                                    [0,1,-1,0,0,0,0], 
                                    [0,0,1,-1,0,0,0], 
@@ -39,22 +40,15 @@ simplerootsE7dim7 = matrix(QQ[sqrt(2)],
                                    [0,0,0,0,1,1,0], 
                                    [-1/2, -1/2, -1/2, -1/2, -1/2, -1/2, sqrt(2)/2]])
 
-simplerootsE8dim8 = matrix(QQ, 
-                          [[1,-1,0,0,0,0,0,0], 
-                          [0,1,-1,0,0,0,0,0], 
-                          [0,0,1,-1,0,0,0,0], 
-                          [0,0,0,1,-1,0,0,0], 
-                          [0,0,0,0,1,-1,0,0], 
-                          [0,0,0,0,0,1,-1,0], 
-                          [0,0,0,0,0,1,1,0], 
-                          [-1/2, -1/2, -1/2, -1/2, -1/2, -1/2, -1/2, -1/2]])
+higherdimroots = [simplerootsG2dim3]  # X's
+lowerdimroots =  [simplerootsG2dim2]  # Y's
 
-higherdimroots = [simplerootsG2dim3]
-lowerdimroots = [simplerootsG2dim2]
+# solve M * X = Y for M
+# currently implemented for G2
 
 for n in range(0,1):
-  X = higherdimroots[n]
-  Y = lowerdimroots[n]
-  M = ( Y * X.transpose() ) * ( X * X.transpose() ).inverse()
-  print(M)
-
+    X = higherdimroots[n]
+    Y = lowerdimroots[n]
+    M = ( Y * X.transpose() ) * (( X * X.transpose() ).inverse())
+    print(M)
+    
