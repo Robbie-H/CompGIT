bc2e7d4fd919e868bcf6956b3804871d4dbb4632
Robbie <robhanson27@gmail.com>
2025-04-04 16:58:40 +0100

    Update projections.py
    Computed projections for E6 and E7

--- projections.py
+++ projections.py
@@ -6,49 +6,75 @@ from sage.all import *
 
 # Choices of simple roots sets for G2, F4, E6, E7, E8 are stored as matrices in which:
 # - columns are choices of simple roots in a specified dimension
-# - in higher dimensional cases, an orthogonal vector is appended in the last column
-# - in lower dimensional cases, a zero vector is appended in the last column
-
-simplerootsG2dim3 = matrix(QQ[sqrt(3)], 
-                           [[1,  -1, 1],
-                            [-1,  2, 1],
-                            [0 , -1, 1]])
-
-simplerootsG2dim2 = matrix(QQ[sqrt(3)], 
-                           [[1, 0, 0],
-                            [-3/2, sqrt(3)/2, 0]])
+# - in higher dimensional cases (denoted X), orthogonal vectors are appended until the matrix is square
+# - in lower dimensional cases (denoted Y), zero vectors are appended until the matrix is square
 
 
-# To-do: input simplerootsE6dim8
+# G2
+simplerootsG2dim3 = matrix(QQ[sqrt(3)],
+                           [[ 1,  -1, 1],
+                            [-1,   2, 1],
+                            [ 0 , -1, 1]])
 
-simplerootsE6dim6 = matrix(QQ[sqrt(3)],
-                                   [[1,-1,0,0,0,0], 
-                                   [0,1,-1,0,0,0], 
-                                   [0,0,1,-1,0,0], 
-                                   [0,0,0,1,-1,0], 
-                                   [0,0,0,1,1,0], 
-                                   [-1/2, -1/2, -1/2, -1/2, -1/2, sqrt(3)/2]])
+simplerootsG2dim2 = matrix(QQ[sqrt(3)],
+                           [[1,    0,         0],
+                            [-3/2, sqrt(3)/2, 0]]
+                            ) # the final column is an orthogonal vector
 
-# To-do: input simplerootsE7dim8
+X = simplerootsG2dim3
+Y = simplerootsG2dim2
+ProjG2 = ( Y * X.transpose() ) * (( X * X.transpose() ).inverse())
+
+
+# E6
+simplerootsE6dim9 = matrix([
+                           [0,  0,  0,  0,  0,  1/3, 1, 0, 0],
+                           [1,  0,  0,  0,  0, -2/3, 1, 0, 0],
+                           [-1, 0,  0,  0,  0,  1/3, 1, 0, 0],
+                           [0,  1,  0,  0,  0, -2/3, 0, 1, 0],
+                           [0, -1,  1,  0,  0,  1/3, 0, 1, 0],
+                           [0,  0, -1,  0,  0,  1/3, 0, 1, 0],
+                           [0,  0,  0,  1,  0, -2/3, 0, 0, 1],
+                           [0,  0,  0, -1,  1,  1/3, 0, 0, 1],
+                           [0,  0,  0,  0, -1,  1/3, 0, 0, 1]
+                           ]) # the final three columns are orthogonal vectors
+
+simplerootsE6dim6 = matrix(QQ[sqrt(3)], [
+                                   [1,    -1,    0,    0,    0,   0,          0, 0, 0],
+                                   [0,     1,   -1,    0,    0,   0,          0, 0, 0],
+                                   [0,     0,    1,   -1,    0,   0,          0, 0, 0],
+                                   [0,     0,    0,    1,   -1,   0,          0, 0, 0],
+                                   [0,     0,    0,    1,    1,   0,          0, 0, 0],
+                                   [-1/2, -1/2, -1/2, -1/2, -1/2, sqrt(3)/2,  0, 0, 0]
+                                   ])
+  
+X = simplerootsE6dim9
+Y = simplerootsE6dim6
+ProjE6 = ( Y * X.transpose() ) * (( X * X.transpose() ).inverse())
+
+
+# E7
+simplerootsE7dim8 = matrix([
+                           [0,  0,  0,  0,   0,  0,  1/2, 1],
+                           [-1, 0,  0,  0,   0,  0,  1/2, 1],
+                           [1, -1,  0,  0,   0,  0,  1/2, 1],
+                           [0,  1, -1,  0,   0,  0,  1/2, 1],
+                           [0,  0,  1, -1,   0,  0, -1/2, 1],
+                           [0,  0,  0,  1,  -1,  0, -1/2, 1],
+                           [0,  0,  0,  0,   1, -1, -1/2, 1],
+                           [0,  0,  0,  0,   0,  1, -1/2, 1]
+                          ]) # the final column is an orthogonal vector
 
 simplerootsE7dim7 = matrix(QQ[sqrt(2)],
-                                   [[1,-1,0,0,0,0,0], 
-                                   [0,1,-1,0,0,0,0], 
-                                   [0,0,1,-1,0,0,0], 
-                                   [0,0,0,1,-1,0,0], 
-                                   [0,0,0,0,1,-1,0], 
-                                   [0,0,0,0,1,1,0], 
-                                   [-1/2, -1/2, -1/2, -1/2, -1/2, -1/2, sqrt(2)/2]])
+                                   [[1,   -1,    0,    0,    0,    0,   0,          0],
+                                   [ 0,    1,   -1,    0,    0,    0,   0,          0],
+                                   [ 0,    0,    1,   -1,    0,    0,   0,          0],
+                                   [ 0,    0,    0,    1,   -1,    0,   0,          0],
+                                   [ 0,    0,    0,    0,    1,   -1,   0,          0],
+                                   [ 0,    0,    0,    0,    1,    1,   0,          0],
+                                   [-1/2, -1/2, -1/2, -1/2, -1/2, -1/2, sqrt(2)/2 , 0]
+                                   ])
 
-higherdimroots = [simplerootsG2dim3]  # X's
-lowerdimroots =  [simplerootsG2dim2]  # Y's
-
-# solve M * X = Y for M
-# currently implemented for G2
-
-for n in range(0,1):
-    X = higherdimroots[n]
-    Y = lowerdimroots[n]
-    M = ( Y * X.transpose() ) * (( X * X.transpose() ).inverse())
-    print(M)
-    
+X = simplerootsE7dim8
+Y = simplerootsE7dim7
+ProjE7 = ( Y * X.transpose() ) * (( X * X.transpose() ).inverse())
